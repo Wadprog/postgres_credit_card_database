09eceb6086bd325d12d18137d5fdf760be384096
Wadson <vavalson@hotmail.com>
2022-11-02 01:28:41 -0400

    created progrma 7 and 8

---
+++ p6.sql
@@ -0,0 +1,26 @@
+/*
+Program 6 charges each vendor a service fee 
+that is 2.5% of the vendor’s balance, ✅
+and subtracts the service fee from the balance. ✅
+The program then displays the name of each vendors, the fee charged, and the new balance.✅
+Date : Tuesday november 1 2022
+Author : Wadson Vaval 
+OWNER : Karla Gomez
+*/
+
+DROP FUNCTION IF EXISTS fn_vendors_service_fee();
+CREATE OR REPLACE FUNCTION fn_vendors_service_fee() 
+RETURNS TABLE(vendor_name VARCHAR, fee numeric, new_balance numeric)
+ LANGUAGE plpgsql AS
+$$
+BEGIN
+ RETURN QUERY
+SELECT 
+V.name AS vendor_name,
+round((2.5* V.balance/100),2)AS fee,
+round((V.balance-(2.5* V.balance/100)),2)AS new_balance
+From vendor AS V;
+END
+$$;
+
+SELECT * FROM fn_vendors_service_fee();
---
+++ p7.sql
@@ -0,0 +1,27 @@
+/*
+Program 7 charges a service fee for each customer 
+whose current balance is greater than the credit limit and ✅
+add the charge to the balance. ✅
+The service fee is 10% of the portion over the credit limit. ✅
+The program then displays the name of each of such customers and the new balance.✅
+Date : Tuesday november 1 2022
+Author : Wadson Vaval 
+OWNER : Karla Gomez
+*/
+
+DROP FUNCTION IF EXISTS fn_vendors_service_fee();
+CREATE OR REPLACE FUNCTION fn_vendors_service_fee() 
+RETURNS TABLE(customer_name VARCHAR, new_balance numeric)
+ LANGUAGE plpgsql AS
+$$
+BEGIN
+ RETURN QUERY
+SELECT 
+C.name AS vendor_name,
+round((C.balance +(10* (C.balance-C.crlimit)/100)),2)AS new_balance
+From customer AS C
+WHERE C.balance > C.crlimit;
+END
+$$;
+
+SELECT * FROM fn_vendors_service_fee();

01759a645df0d6234d16d482658eee069f1943a5
Wadson <vavalson@hotmail.com>
2022-11-02 22:40:10 -0400

    fixed p4 and p8

--- p4.sql
+++ p4.sql
@@ -1,35 +1,30 @@
 /*
-
 Program 4 displays the most recent transaction of every customer.
 The program displays account number, customer name, amount, and vendor name.
 If a customer has no transaction (e.g. the new one), the program should display “no transaction”.
+Date : Tuesday november 1 2022
+Author : Wadson Vaval 
+OWNER : Karla Gomez
 */
 
 DROP FUNCTION IF EXISTS fn_cst_recent_transaction();
 CREATE OR REPLACE FUNCTION fn_cst_recent_transaction() 
-RETURNS TABLE(account_number INT, customer_name VARCHAR, amount INT, vendor_name VARCHAR)
+RETURNS TABLE(account_number INT, customer_name VARCHAR, amount TEXT)
  LANGUAGE plpgsql AS
 $$
 BEGIN
  RETURN QUERY
-  SELECT 
+  
+ SELECT 
   C.account_number,
-  C.name AS customer_name, 
-  (
-  SELECT amount
-	FROM transaction
-	--INNER JOIN customer AS C on transaction.account_number=C.account_number
-   WHERE transaction.account_number=C.account_number
-   GROUP BY transaction.account_number
-   ORDER BY transaction.date DESC
-  
-   --LIMIT 1
-  )AS amount
-  FROM customer AS C
-  
-END
+  C.name AS customer_name,
+  COALESCE(MAX(T.amount)::TEXT,'no transaction')AS transaction_amount,
+  FROM transaction AS T
+  RIGHT JOIN customer AS C  USING(account_number)
+  --INNER JOIN vendor AS V USING(vendor_number)
+  GROUP BY T.account_number, C.account_number,C.name;
+END;
 $$;
 
 -- Calling the function 
-SELECT * FROM fn_display_customer_transaction('Smith');
-SELECT * FROM customer
+SELECT * FROM fn_cst_recent_transaction();

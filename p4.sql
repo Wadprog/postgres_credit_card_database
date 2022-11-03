1b851296e569b4e26a4b147ef279ef00683d4f32
Wadson <vavalson@hotmail.com>
2022-11-02 23:03:54 -0400

    remove unessary comment

--- p4.sql
+++ p4.sql
@@ -2,7 +2,7 @@
 Program 4 displays the most recent transaction of every customer.
 The program displays account number, customer name, amount, and vendor name.
 If a customer has no transaction (e.g. the new one), the program should display “no transaction”.
-Date : Tuesday november 1 2022
+Date : Tuesday November 2 2022
 Author : Wadson Vaval 
 OWNER : Karla Gomez
 */
@@ -13,15 +13,12 @@ RETURNS TABLE(account_number INT, customer_name VARCHAR, amount TEXT)
  LANGUAGE plpgsql AS
 $$
 BEGIN
- RETURN QUERY
-  
- SELECT 
+ RETURN QUERY SELECT 
   C.account_number,
   C.name AS customer_name,
   COALESCE(MAX(T.amount)::TEXT,'no transaction')AS transaction_amount,
   FROM transaction AS T
   RIGHT JOIN customer AS C  USING(account_number)
-  --INNER JOIN vendor AS V USING(vendor_number)
   GROUP BY T.account_number, C.account_number,C.name;
 END;
 $$;
